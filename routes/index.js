625490929c6a3bf1f1b56dc3ed6e006ca3707e2b
CalebF98 <cfringer98@gmail.com>
2023-06-28 19:52:33 -0700

    Implemented input sanitization

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,20 @@
 const express = require('express');
 const router = express.Router();
-const validate = require('../controllers/validate');
+const sanitize = require('../controllers/sanitize');
+const parse = require('../controllers/parse');
+const { validationResult } = require('express-validator');
 
 router.get('/', (req, res) => {
     res.render('index.ejs');
 });
 
-router.post('/', validate, (req, res) => {
-    res.render('upload.ejs');
+router.post('/', [sanitize, parse] , (req, res) => {
+    const err = validationResult(req);
+    if (!err.isEmpty()) {
+        return res.status(422).json({ err: err.array() });
+    }
+    console.log(res.records);
+    res.render('upload.ejs', { softreserves: res.records });
 });
 
 module.exports = router;
