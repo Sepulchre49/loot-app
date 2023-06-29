64f2adc54a97ed4278e2d189017575c64fbc8604
CalebF98 <cfringer98@gmail.com>
2023-06-28 19:50:50 -0700

    Set up database connection

--- app.js
+++ app.js
@@ -1,18 +1,28 @@
 const express = require('express');
 const app = express();
-const ejs = require('ejs');
 const index = require('./routes/index');
-const validate = require('./controllers/validate');
+const dotenv = require('dotenv');
+const mongoose = require('mongoose');
 
+// Config environment
+dotenv.config();
 const port = process.env.PORT || 3333;
 
-app.set('view engine', 'ejs');
+// Connect to db
+(async () => {
+    await mongoose.connect(process.env.ATLAS_URI);
+})()
+    .then(() => console.log("Successfully connected to database!"))
+    .catch(err => console.log(err));
 
+// Set up middleware and view engine
+app.set('view engine', 'ejs');
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
-
+// Routing middleware
 app.use('/', index);
 
+// Start app
 app.listen(port, () => {
     console.log(`Listening on port ${port}...`);
 });
