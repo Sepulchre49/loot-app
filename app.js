8c360c1720ac4c06bbed1c75cafb9ffc44800b8c
CalebF98 <cfringer98@gmail.com>
2023-06-28 20:13:07 -0700

    Refactored startup code so server only starts if database connection was successful

--- app.js
+++ app.js
@@ -8,12 +8,6 @@ const mongoose = require('mongoose');
 dotenv.config();
 const port = process.env.PORT || 3333;
 
-// Connect to db
-(async () => {
-    await mongoose.connect(process.env.ATLAS_URI);
-})()
-    .then(() => console.log("Successfully connected to database!"))
-    .catch(err => console.log(err));
 
 // Set up middleware and view engine
 app.set('view engine', 'ejs');
@@ -22,7 +16,19 @@ app.use(express.urlencoded({extended: true}));
 // Routing middleware
 app.use('/', index);
 
-// Start app
-app.listen(port, () => {
-    console.log(`Listening on port ${port}...`);
-});
+
+async function startApp() {
+    // Connect to db
+    try {
+        await mongoose.connect(process.env.ATLAS_URI);
+        console.log("Successfully connected to database!");
+    } catch (err) {
+        return console.log(err);
+    }
+    // Start web server
+    app.listen(port, () => {
+        console.log(`Listening on port ${port}...`);
+    });
+}
+
+startApp();
