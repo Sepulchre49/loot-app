ab1916ad363acf89657bc02efdb62630f6f1543f
CalebF98 <cfringer98@gmail.com>
2023-06-28 19:52:07 -0700

    Middleware for parsing uploaded csv

---
+++ controllers/parse.js
@@ -0,0 +1,11 @@
+const { parse } = require('csv-parse/sync');
+
+module.exports = function (req, res, next) {
+    res.records = parse(req.body.softreserves, {
+        columns: true
+    });
+    res.records.forEach(reserve => {
+        
+    });
+    next();
+}
