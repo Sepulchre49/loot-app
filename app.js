41be829be9ed36fbc2634f74ebcf06f17ca88235
CalebF98 <cfringer98@gmail.com>
2023-06-30 19:33:27 -0700

    Needed to up the upload limit to upload p2 data

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ const port = process.env.PORT || 3000;
 // Set up middleware and view engine
 app.set('view engine', 'ejs');
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({limit: "1mb", extended: true}));
 // Routing middleware
 app.use('/', index);
 
